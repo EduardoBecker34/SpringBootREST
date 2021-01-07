d0223db0efb086341acd17ad0bef493926db24e4
EduardoBecker <eduardobeckerdaluz@gmail.com>
2021-01-06 22:54:25 -0300

    SQLs para execução de migrations.

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `addres`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Avenida Brasil', 'Eduardo', 'Male', 'Becker'),
+	(2, 'Bento Gonçalves', 'Alison', 'Male', 'Panisson'),
+	(3, 'Eduardo de Britto', 'Raymond', 'Male', 'Stantz'),
+	(4, 'Eduardo de Britto', 'Egon', 'Male', 'Spengler'),
+	(5, 'Eduardo de Britto', 'Egon', 'Male', 'Spengler');
