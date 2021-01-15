435408728d639e9a3ee90fd4c76a04e603506741
EduardoBecker <eduardobeckerdaluz@gmail.com>
2021-01-14 21:44:30 -0300

    Correção de migrations.

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,4 +1,4 @@
-INSERT INTO `person` (`id`, `addres`, `first_name`, `gender`, `last_name`) VALUES
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
 	(1, 'Avenida Brasil', 'Eduardo', 'Male', 'Becker'),
 	(2, 'Bento Gonçalves', 'Alison', 'Male', 'Panisson'),
 	(3, 'Eduardo de Britto', 'Raymond', 'Male', 'Stantz'),
--- src/main/resources/db/migration/v1__Create_Table_Person.sql
+++ src/main/resources/db/migration/v1__Create_Table_Person.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `person` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `addres` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
   `first_name` varchar(80) NOT NULL,
   `gender` varchar(6) NOT NULL,
   `last_name` varchar(80) NOT NULL,
